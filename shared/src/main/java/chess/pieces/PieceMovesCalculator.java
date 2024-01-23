ab0bf30d457d1ce63b8fcc20e7b959149b9fb82f
nraustin <nickademic@gmail.com>
2024-01-22 20:03:34 -0700

    PawnMoves complete

--- shared/src/main/java/chess/pieces/PieceMovesCalculator.java
+++ shared/src/main/java/chess/pieces/PieceMovesCalculator.java
@@ -60,6 +60,12 @@ public interface PieceMovesCalculator {
 
             ChessPosition newPosition = new ChessPosition(row + moves[i][0], col + moves[i][1]);
 
+            boolean promotion = newPosition.getRow() == 8 && board.getPiece(startPosition).getTeamColor() == ChessGame.TeamColor.WHITE ||
+                                newPosition.getRow() == 1 && board.getPiece(startPosition).getTeamColor() == ChessGame.TeamColor.BLACK;
+
+            boolean firstTurn = startPosition.getRow() == 2 && board.getPiece(startPosition).getTeamColor() == ChessGame.TeamColor.WHITE ||
+                                startPosition.getRow() == 7 && board.getPiece(startPosition).getTeamColor() == ChessGame.TeamColor.BLACK;
+
             if (newPosition.validPosition()) {
                 // Pawn case
                 if (board.getPiece(startPosition).getPieceType() == ChessPiece.PieceType.PAWN) {
@@ -68,21 +74,39 @@ public interface PieceMovesCalculator {
                     // Forward advancement
                     if(moves[i][1] == 0){
                         if(board.getPiece(newPosition) == null){
-                            ChessMove singleForward = new ChessMove(startPosition, newPosition, null);
-                            legalMoves.add(singleForward);
-                            // Check for open space for double advancement
-                            ChessPosition twoSquaresAhead = new ChessPosition(newPosition.getRow()+direction, newPosition.getColumn());
-                            if(board.getPiece(twoSquaresAhead) == null){
-                                ChessMove doubleForward = new ChessMove(startPosition, twoSquaresAhead, null);
-                                legalMoves.add(doubleForward);
+                            if(promotion){
+                                legalMoves.add(new ChessMove(startPosition, newPosition, ChessPiece.PieceType.ROOK));
+                                legalMoves.add(new ChessMove(startPosition, newPosition, ChessPiece.PieceType.KNIGHT));
+                                legalMoves.add(new ChessMove(startPosition, newPosition, ChessPiece.PieceType.BISHOP));
+                                legalMoves.add(new ChessMove(startPosition, newPosition, ChessPiece.PieceType.QUEEN));
                             }
+                            else{
+                                ChessMove singleForward = new ChessMove(startPosition, newPosition, null);
+                                legalMoves.add(singleForward);
+                                // Check for open space for double advancement
+                                System.out.println(firstTurn);
+                                ChessPosition twoSquaresAhead = new ChessPosition(newPosition.getRow()+direction, newPosition.getColumn());
+                                if (firstTurn && board.getPiece(twoSquaresAhead) == null){
+                                    ChessMove doubleForward = new ChessMove(startPosition, twoSquaresAhead, null);
+                                    legalMoves.add(doubleForward);
+                                }
+                            }
+
                         }
                     }
                     // Diagonal enemy capture
                     else {
                         if(board.getPiece(newPosition) != null && board.getPiece(newPosition).getTeamColor() != board.getPiece(startPosition).getTeamColor()){
-                            ChessMove captureDiagonal = new ChessMove(startPosition, newPosition, null);
-                            legalMoves.add(captureDiagonal);
+                            if(promotion){
+                                legalMoves.add(new ChessMove(startPosition, newPosition, ChessPiece.PieceType.ROOK));
+                                legalMoves.add(new ChessMove(startPosition, newPosition, ChessPiece.PieceType.KNIGHT));
+                                legalMoves.add(new ChessMove(startPosition, newPosition, ChessPiece.PieceType.BISHOP));
+                                legalMoves.add(new ChessMove(startPosition, newPosition, ChessPiece.PieceType.QUEEN));
+                            }
+                            else{
+                                ChessMove captureDiagonal = new ChessMove(startPosition, newPosition, null);
+                                legalMoves.add(captureDiagonal);
+                            }
                         }
                     }
 
