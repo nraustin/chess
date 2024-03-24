077fbe78cf448f34ad2fd6445031f80a39cc86e4
nraustin <nickademic@gmail.com>
2024-03-24 14:01:42 -0600

    Progress on universal http req/res handler for server facade

--- client/src/main/java/web/ServerFacade.java
+++ client/src/main/java/web/ServerFacade.java
@@ -1,8 +1,15 @@
 package web;
 
+import com.google.gson.Gson;
 import ui.EscapeSequences;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.*;
+import java.util.Objects;
 import java.util.Scanner;
+
+import exception.ResponseException;
 
 public class ServerFacade {
 
@@ -12,5 +19,28 @@ public class ServerFacade {
         this.serverURL = serverURL;
     }
 
+    private <T> T httpHandler(String method, String endpoint, Object request, Class<T> resClass) throws ResponseException {
+        try {
+            // 1 create URL instance
+            URL url = new URI(serverURL + endpoint).toURL();
+            // 2 open connection
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            // set request method
+            connection.setRequestMethod(method);
+            // post request
+            if(request != null){
+                connection.setDoOutput(true);
+                OutputStream os = connection.getOutputStream();
+                writeReqBody(os, request);
+            }
 
+        } catch (Exception e){
+            throw new ResponseException(500, e.getMessage());
+        }
+    }
+
+    private void writeReqBody(OutputStream os, Object request) throws IOException {
+        String req = new Gson().toJson(request);
+        os.write(req.getBytes());
+    }
 }
