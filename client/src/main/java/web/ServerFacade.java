62e96002d184bff2a97701d8ec846ec2a57a63cc
nraustin <nickademic@gmail.com>
2024-03-24 23:59:29 -0600

    httpHandler complete

--- client/src/main/java/web/ServerFacade.java
+++ client/src/main/java/web/ServerFacade.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import ui.EscapeSequences;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.*;
 import java.util.Objects;
@@ -14,6 +16,7 @@ import exception.ResponseException;
 public class ServerFacade {
 
     private final String serverURL;
+    private String authToken;
 
     public ServerFacade(String serverURL){
         this.serverURL = serverURL;
@@ -21,26 +24,51 @@ public class ServerFacade {
 
     private <T> T httpHandler(String method, String endpoint, Object request, Class<T> resClass) throws ResponseException {
         try {
-            // 1 create URL instance
             URL url = new URI(serverURL + endpoint).toURL();
-            // 2 open connection
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            // set request method
             connection.setRequestMethod(method);
-            // post request
+            connection.setDoOutput(request == null ? false : true);
+
+            if(authToken != null){
+                connection.addRequestProperty("Authorization", authToken);
+            }
+
             if(request != null){
-                connection.setDoOutput(true);
+                connection.addRequestProperty("Content-Type", "application/json");
                 OutputStream os = connection.getOutputStream();
                 writeReqBody(os, request);
             }
 
+            connection.connect();
+
+            int status = connection.getResponseCode();
+            if(status != 200){
+                throw new ResponseException(status, String.format("Failure: %s", connection.getResponseMessage()));
+            }
+
+            return readResBody(connection, resClass);
         } catch (Exception e){
             throw new ResponseException(500, e.getMessage());
         }
     }
 
     private void writeReqBody(OutputStream os, Object request) throws IOException {
-        String req = new Gson().toJson(request);
-        os.write(req.getBytes());
+        String reqData = new Gson().toJson(request);
+        os.write(reqData.getBytes());
     }
+
+    private static <T> T readResBody (HttpURLConnection connection, Class<T> resClass) throws IOException {
+        T res = null;
+        if(connection.getContentLength() > 0){
+            try (InputStream resBody = connection.getInputStream()){
+                InputStreamReader reader = new InputStreamReader(resBody);
+                if(resClass != null) {
+                    res = new Gson().fromJson(reader, resClass);
+                }
+            }
+        }
+        return res;
+    }
+
+
 }
